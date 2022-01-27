43c890c38a890082398aba6dcddabc026144f18c
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-27 08:39:16 -0300

    Refactor: replace @PersistenceContext with a constructor and @Autowired in UsuarioDao

--- src/main/java/com/ndrewcoding/leilao/dao/UsuarioDao.java
+++ src/main/java/com/ndrewcoding/leilao/dao/UsuarioDao.java
@@ -1,16 +1,20 @@
 package com.ndrewcoding.leilao.dao;
 
 import com.ndrewcoding.leilao.model.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 @Repository
 public class UsuarioDao {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
+
+    @Autowired
+    public UsuarioDao(EntityManager em) {
+        this.em = em;
+    }
 
     public Usuario buscarPorUsername(String username) {
         return em.createQuery("SELECT u FROM Usuario u WHERE u.nome = :username", Usuario.class)
