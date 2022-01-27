1a16b20e360758ad4f1c3fbcdf8de1060c4a7775
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-27 20:20:22 -0300

    Test: set up basic structure of LeilaoDaoTest

--- src/test/java/com/ndrewcoding/leilao/dao/LeilaoDaoTest.java
+++ src/test/java/com/ndrewcoding/leilao/dao/LeilaoDaoTest.java
@@ -1,4 +1,28 @@
 package com.ndrewcoding.leilao.dao;
 
+import com.ndrewcoding.leilao.util.JPAUtil;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import javax.persistence.EntityManager;
+
 public class LeilaoDaoTest {
+
+    private LeilaoDao leilaoDao;
+    private EntityManager entityManager;
+
+    @BeforeEach
+    void beforeEach() {
+        this.entityManager = JPAUtil.getEntityManager();
+
+        this.leilaoDao = new LeilaoDao(entityManager);
+
+        this.entityManager.getTransaction().begin();
+    }
+
+    @AfterEach
+    void afterEach() {
+        this.entityManager.getTransaction().rollback();
+    }
+
 }
