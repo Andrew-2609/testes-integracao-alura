112c3cee4699d335b6576c15387ed8ac7458787a
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-27 09:00:08 -0300

    Test: implement test 'testeBuscaDeUsuarioPeloUsername' in UsuarioDaoTest

--- src/test/java/com/ndrewcoding/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/com/ndrewcoding/leilao/dao/UsuarioDaoTest.java
@@ -1,4 +1,31 @@
 package com.ndrewcoding.leilao.dao;
 
+import com.ndrewcoding.leilao.model.Usuario;
+import com.ndrewcoding.leilao.util.JPAUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.EntityManager;
+
 public class UsuarioDaoTest {
+
+    private UsuarioDao usuarioDao;
+
+    @Test
+    void testeBuscaDeUsuarioPeloUsername() {
+        EntityManager entityManager = JPAUtil.getEntityManager();
+
+        this.usuarioDao = new UsuarioDao(entityManager);
+
+        Usuario novoUsuario = new Usuario("Andrew", "andrew@email.com", "13579");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(novoUsuario);
+        entityManager.getTransaction().commit();
+
+        Usuario usuarioEncontrado = this.usuarioDao.buscarPorUsername(novoUsuario.getNome());
+
+        Assertions.assertNotNull(usuarioEncontrado);
+    }
+
 }
