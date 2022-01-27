8bce6ba81912b18bacfb19d95aa90b3a2cee8735
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-27 08:45:30 -0300

    Feat: implement an EntityManagerFactory and a method to retrieve this created EntityManager in JPAUtil

--- src/test/java/com/ndrewcoding/leilao/util/JPAUtil.java
+++ src/test/java/com/ndrewcoding/leilao/util/JPAUtil.java
@@ -1,4 +1,15 @@
 package com.ndrewcoding.leilao.util;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class JPAUtil {
+
+    private final static EntityManagerFactory factory = Persistence.createEntityManagerFactory("tests");
+
+    public static EntityManager getEntityManager() {
+        return factory.createEntityManager();
+    }
+
 }
