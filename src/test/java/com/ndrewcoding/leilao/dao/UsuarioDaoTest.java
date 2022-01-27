7e5cf8f105418f449fe25d69e246ddfd905393e7
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-27 09:05:44 -0300

    Test: implement test 'naoDeveriaEncontrarUsuarioNaoCadastrado' in UsuarioDaoTest

--- src/test/java/com/ndrewcoding/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/com/ndrewcoding/leilao/dao/UsuarioDaoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 
 public class UsuarioDaoTest {
 
@@ -26,6 +27,21 @@ public class UsuarioDaoTest {
         Usuario usuarioEncontrado = this.usuarioDao.buscarPorUsername(novoUsuario.getNome());
 
         Assertions.assertNotNull(usuarioEncontrado);
+    }
+
+    @Test
+    void naoDeveriaEncontrarUsuarioNaoCadastrado() {
+        EntityManager entityManager = JPAUtil.getEntityManager();
+
+        this.usuarioDao = new UsuarioDao(entityManager);
+
+        Usuario novoUsuario = new Usuario("Andrew", "andrew@email.com", "13579");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(novoUsuario);
+        entityManager.getTransaction().commit();
+
+        Assertions.assertThrows(NoResultException.class, () -> this.usuarioDao.buscarPorUsername("Ndrew"));
     }
 
 }
