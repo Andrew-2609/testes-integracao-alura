30ce2e5337df020338712451bab67c876daf6432
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-27 22:01:38 -0300

    Feat: implement basic structure of UsuarioBuilder

--- src/test/java/com/ndrewcoding/leilao/util/builder/UsuarioBuilder.java
+++ src/test/java/com/ndrewcoding/leilao/util/builder/UsuarioBuilder.java
@@ -1,4 +1,24 @@
 package com.ndrewcoding.leilao.util.builder;
 
 public class UsuarioBuilder {
+
+    private String nome;
+    private String email;
+    private String senha;
+
+    public UsuarioBuilder comNome(String nome) {
+        this.nome = nome;
+        return this;
+    }
+
+    public UsuarioBuilder comEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public UsuarioBuilder comSenha(String senha) {
+        this.senha = senha;
+        return this;
+    }
+
 }
