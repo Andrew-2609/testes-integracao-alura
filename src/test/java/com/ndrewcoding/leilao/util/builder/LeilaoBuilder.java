c7813b2ed4d4a2bd05ef6029f7aa4cb0dcbd9705
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-27 21:57:26 -0300

    Feat: implement basic structure of LeilaoBuilder

--- src/test/java/com/ndrewcoding/leilao/util/builder/LeilaoBuilder.java
+++ src/test/java/com/ndrewcoding/leilao/util/builder/LeilaoBuilder.java
@@ -1,4 +1,28 @@
 package com.ndrewcoding.leilao.util.builder;
 
+import com.ndrewcoding.leilao.model.Usuario;
+
+import java.math.BigDecimal;
+
 public class LeilaoBuilder {
+
+    private String nome;
+    private BigDecimal valorInicial;
+    private Usuario usuario;
+
+    public LeilaoBuilder comNome(String nome) {
+        this.nome = nome;
+        return this;
+    }
+
+    public LeilaoBuilder comValorInicial(String valor) {
+        this.valorInicial = new BigDecimal(valor);
+        return this;
+    }
+
+    public LeilaoBuilder comUsuario(Usuario usuario) {
+        this.usuario = usuario;
+        return this;
+    }
+
 }
